cea6459bb3c01b2c1361e8f5a14498c188a9bb8e
ogs1492 <36433418+ogs1492@users.noreply.github.com>
2021-04-05 18:21:29 -0700

    Completely changed whole file
    Redid the entire thing to be more readable and fixed all of the bugs.

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -1,150 +1,136 @@
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 
-class Window(Frame):
-
-    def __init__(self, master = None):
-        Frame.__init__(self, master)
-        self.master = master
-        self.pack(fill = BOTH, expand = 1)
-
-def checkWin():
-    checkDiagonals()
-    checkRows()
-    checkColumns()
-
-def checkDiagonals():
-    if board[0][0] == board[1][1] == board[2][2] != 0:
-        win(board[0][0])
-    if board[0][2] == board[1][1] == board[2][0] != 0:
-        win(board[2][0])
-
-def checkRows():
-    if board[0][0] == board[0][1] == board[0][2] != 0:
-        win(board[0][0])
-    if board[1][0] == board[1][1] == board[1][2] != 0:
-        win(board[1][0])
-    if board[2][0] == board[2][1] == board[2][2] != 0:
-        win(board[2][0])
-
-def checkColumns():
-    if board[0][0] == board[1][0] == board[2][0] != 0:
-        win(board[0][0])
-    if board[0][1] == board[1][1] == board[2][1] != 0:
-        win(board[0][1])
-    if board[0][2] == board[1][2] == board[2][2] != 0:
-        win(board[0][2])
-
-def win(player):
-    if player == 1:
-        global playerOneWins
-        global p1counter
-        print("Player One Wins")
-        playerOneWins += 1
-        p1counter.configure(text = playerOneWins)
-    if player == 2:
-        global playerTwoWins
-        global p2counter
-        print("Player Two Wins")
-        playerTwoWins += 1
-        p2counter.configure(text = playerTwoWins)
-    reset()
-
-def click(btnid, row, column):
-    global turn
-    if turn % 2 == 0:
-        btnid.configure(image = xImage)
-        turn += 1
-        board[row][column] = 1
-    else:
-        btnid.configure(image = oImage)
-        turn += 1
-        board[row][column] = 2
-    btnid.configure(command = NONE)
-    checkWin()
-
-def reset():
-    board = [[0,0,0], [0,0,0], [0,0,0]]
-    for sq in squares:
-        sq.configure(image = blank)
-
+# Initialize so images can load
+root = tk.Tk()
 turn = 0
-root = Tk()
-window = Window(root)
-root.wm_title("Tic tac toe")
-root.geometry("400x600")
 
-board = [[0,0,0], [0,0,0], [0,0,0]]
+# Class that wraps the functionality of each square
+class Square(tk.Button):
+    def __init__(self, game, x, y):
+        self.game = game
+        self.value = 0;
+        super().__init__(root, bd = 0)
+        super().configure(command = self.click, image = self.blankImage)
+        super().place(x = x, y = y)
 
-playerOneWins = 0
-playerTwoWins = 0
+    def click(self):
+        if self.clicked == False:
+            global turn
+            if turn % 2 == 0:
+                self.value = 1
+            else:
+                self.value = 2
+            self.draw();
+            self.clicked = True
+            turn += 1
+            self.game.checkWin()
+            
+    def draw(self):
+            if self.value == 0:
+               super().configure(image = self.blankImage)
+            elif self.value == 1:
+               super().configure(image = self.xImage)
+            elif self.value == 2:
+               super().configure(image = self.oImage)        
+    
+    def reset(self):
+        self.value = 0
+        self.clicked = False
+        self.draw()
 
-xImage = ImageTk.PhotoImage(Image.open("x.png"))
-oImage = ImageTk.PhotoImage(Image.open("o.png"))
-blank = ImageTk.PhotoImage(Image.open("blank.png"))
+    game = None
+    # The images used for the squares
+    blankImage = ImageTk.PhotoImage(Image.open("blank.png"))
+    xImage = ImageTk.PhotoImage(Image.open("x.png"))
+    oImage = ImageTk.PhotoImage(Image.open("o.png"))
 
-p1 = Label(root, text = "Player 1:", font = ("Helvetica", 10))
-p1.place(x = 310, y = 0)
+    # The current state of the square. 0 = blank. 1 = X (player 1). 2 = O (player 2)
+    value = 0
+    clicked = False
 
-p1counter = Label(root, text = "0", font = ("Helvetica", 20))
-p1counter.place(x = 340, y = 30)
+class Game(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.oo = Square(self, 0, 0)
+        self.ot = Square(self, 0, 100)
+        self.oh = Square(self, 0, 200)
+        self.to = Square(self, 100, 0)
+        self.tt = Square(self, 100, 100)
+        self.th = Square(self, 100, 200)
+        self.ho = Square(self, 200, 0)
+        self.ht = Square(self, 200, 100)
+        self.hh = Square(self, 200, 200)
+        self.board = [[self.oo, self.ot, self.oh], [self.to, self.tt, self.th], [self.ho, self.ht, self.hh]]
+        self.p1Label.place(x = 300, y = 0)
+        self.p1ScoreLabel.place(x = 300, y = 30)
+        self.p2Label.place(x = 300, y = 100)
+        self.p2ScoreLabel.place(x = 300, y = 130)
+        super().pack()
 
-p2 = Label(root, text = "Player 2:", font = ("Helvetica", 10))
-p2.place(x = 310, y = 100)
+    def updateLabels(self):
+        self.p1ScoreLabel.config(text = str(self.p1Score))
+        self.p2ScoreLabel.config(text = str(self.p2Score))
+        
+    def checkColumns(self):
+        for r in self.board:
+            if r[0].value == r[1].value == r[2].value != 0:
+                self.win = True
+    
+    def checkRows(self):
+        for i in range(0, 3):
+            if self.board[0][i].value == self.board[1][i].value == self.board[2][i].value != 0:
+                self.win = True
+    
+    def checkDiagonals(self):
+        if self.board[0][0].value == self.board[1][1].value == self.board[2][2].value != 0 or \
+           self.board[2][0].value == self.board[1][1].value == self.board[0][2].value != 0:
+            self.win = True
 
-p2counter = Label(root, text = "0", font = ("Helvetica", 20))
-p2counter.place(x = 340, y = 130)
+    def checkWin(self):
+        global turn
+        self.checkColumns()
+        self.checkRows()
+        self.checkDiagonals()
+        if self.win == True:
+            
+            if turn % 2 == 1:
+                self.p1Score += 1
+            else:
+                self.p2Score += 1
+            turn = 0
+            self.win = False
+            self.updateLabels()
+            for r in self.board:
+                for c in r:
+                    c.reset()
 
+    win = False
+    # Squares used for the gameboard. o = One, t = Two, h = Three
+    oo = None
+    ot = None
+    oh = None
+    to = None
+    tt = None
+    th = None
+    ho = None
+    ht = None
+    hh = None
+    # The gameboard as an array
+    board = []
+    # Player score and counters
+    p1Score = 0
+    p1Label = tk.Label(root, text = "Player 1:")
+    p1ScoreLabel = tk.Label(root, text = "0", font = ("Arial", 25))
+    p2Score = 0
+    p2Label = tk.Label(root, text = "Player 2:")
+    p2ScoreLabel = tk.Label(root, text = "0", font = ("Arial", 25))
 
-
-global oo
-oo = Button(root)
-oo.configure(command = lambda id = oo:  click(btnid = id, row = 0, column=0))
-oo.place(x=0, y=0)
-
-global ot
-ot = Button(root)
-ot.configure(command = lambda id=ot: click(btnid = id, row = 1, column=0))
-ot.place(x=0, y=100)
-
-global oh
-oh = Button(root)
-oh.configure(command = lambda id=oh: click(btnid = id, row = 2, column=0))
-oh.place(x=0, y=200)
-
-global to
-to = Button(root)
-to.configure(command = lambda id=to: click(btnid = id,  row = 0, column=1))
-to.place(x=100, y=0)
-
-global tt
-tt = Button(root)
-tt.configure(command = lambda id=tt: click(btnid = id, row = 1, column=1))
-tt.place(x=100, y=100)
-
-global th
-th = Button(root)
-th.configure(command = lambda id=th: click(btnid = id, row = 2, column=1))
-th.place(x=100, y=200)
-
-global ho
-ho = Button(root)
-ho.configure(command = lambda id=ho: click(btnid = id, row = 0, column=2))
-ho.place(x=200, y=0)
-
-global ht
-ht = Button(root)
-ht.configure(command = lambda id=ht: click(btnid = id, row = 1, column=2))
-ht.place(x=200, y=100)
-
-global hh
-hh = Button(root)
-hh.configure(command = lambda id=hh: click(btnid = id, row = 2, column=2))
-hh.place(x=200, y=200)
-
-squares = [oo, ot, oh, to, tt, th, ho, ht, hh]
-
-for sq in squares:
-    sq.configure(height = 100, width = 100, image = blank, relief = FLAT)
-
-root.mainloop()
+    
+# Actual initalization of the game
+root.geometry("400x300")
+root.wm_title("Tic-tac-toe")
+global game
+game = Game(master=root)
+game.mainloop()
